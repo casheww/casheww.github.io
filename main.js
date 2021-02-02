a93bd0381ef7b216f76d218709b0c31f2fea8dc2
casheww <51793722+casheww@users.noreply.github.com>
2021-02-02 23:45:04 +0000

    restyle

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function getGithubStuff() {
+    let client = new XMLHttpRequest();
+    client.open("GET", "https://api.github.com/users/casheww");
+
+    client.onload = function() {
+        if (this.status === 200) {
+            var data = JSON.parse(this.responseText);
+            document.getElementById("github-pfp").innerHTML = `<img src="${data["avatar_url"]}">`;
+            document.getElementById("github-bio").innerText = data["bio"]
+        }
+    }
+    client.send();
+}
+
+function stuff() {
+    console.log("What are you doing here?");
+
+    getGithubStuff();
+    
+}
+
