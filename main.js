3622abe0479d179c111f73163e3b078c9443ceaf
casheww <51793722+casheww@users.noreply.github.com>
2021-08-26 23:53:29 +0100

    added navigator

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 
-/*
-function getGithubStuff() {
-    let client = new XMLHttpRequest();
-    client.open("GET", "https://api.github.com/users/casheww");
+function setPageLocationBanner () {
+    var p = document.createElement("p");
+    p.className = "js-location";
 
-    client.onload = function() {
-        if (this.status === 200) {
-            var data = JSON.parse(this.responseText);
-            document.getElementById("github-pfp").innerHTML = `<img src="${data["avatar_url"]}">`;
-            document.getElementById("github-bio").innerText = data["bio"]
-        }
-    }
-    client.send();
-}
+    var l = window.location.pathname.split("/");
+    var pathBuilder = window.location.origin;
 
-function stuff() {
-    console.log("What are you doing here?");
+    var rootA = document.createElement("a");
+    rootA.href = pathBuilder;
+    rootA.text = "~";
+    p.appendChild(rootA);
 
-    getGithubStuff();
+    l.forEach(element => {
+        if (element != "") {
+            var a = document.createElement("a");
+            pathBuilder = `${pathBuilder}/${element}`;
     
+            a.href = pathBuilder;
+            a.text = element;
+    
+            p.appendChild(document.createTextNode(" / "));
+            p.appendChild(a);
+        }
+    });
+
+    document.body.insertBefore(p, document.getElementById("main"));
 }
-*/
+
+
+function setDirectoryContentsList () {
+
+}
+
+
+window.addEventListener('load', (event) => {
+    setPageLocationBanner();
+    setDirectoryContentsList();
+    console.log("done");
+});
