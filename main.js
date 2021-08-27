79823edc27ae659e9d4df054bd4ae121fff8a166
casheww <51793722+casheww@users.noreply.github.com>
2021-08-27 01:14:04 +0100

    added folder js

--- main.js
+++ main.js
@@ -29,7 +29,37 @@ function setPageLocationBanner () {
 
 
 function setDirectoryContentsList () {
+    var dirList = document.getElementById("dir");
+    if (dirList === null) {
+        console.log("not a dir");
+        return;
+    }
 
+    let client = new XMLHttpRequest();
+    client.open("GET", "/files.json");
+
+    client.onload = function () {
+        if (this.status === 200) {
+            var data = JSON.parse(this.responseText);
+
+            console.log(data);
+
+            var dir = window.location.pathname.replace("/", "").replace("/", "");
+            dir = "./" + dir;
+
+            data[dir].forEach(element => {
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                a.href = element;
+                a.text = element;
+                li.appendChild(a);
+                dirList.appendChild(li);
+            });
+
+        }
+    }
+    
+    client.send();
 }
 
 
